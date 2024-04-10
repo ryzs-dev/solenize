3570b8d6ca741d9a13c25b32a17774108bdb8a2d
Ryan Zs <145740203+ryzs-dev@users.noreply.github.com>
2024-04-11 01:58:28 +0800

    01-Hover_Effect_Change

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
-import Image from "next/image"
+import Image from "next/image";
 
 export default function Home() {
   return (
-   <main className="bg-my_bg_image h-screen w-screen bg-no-repeat bg-cover">
-    <div className="absolute right-0 bottom-0 ">
-      <Image src="/logo 3.png" alt="bg-image" width={600} height={500} />
-    </div>
-     <div className="flex px-[60px] py-0 items-center gap-[163px] self-stretch pt-[220px]">
+    <main className="bg-my_bg_image h-screen w-screen bg-no-repeat bg-cover">
+      <div className="absolute right-0 bottom-0 ">
+        <Image src="/logo 3.png" alt="bg-image" width={600} height={500} />
+      </div>
+      <div className="flex px-[60px] py-0 items-center gap-[163px] self-stretch pt-[220px]">
         <div className="flex flex-col items-start gap-[30px]">
           <div className="flex flex-col items-start gap-[30px]">
             <div className="flex w-[607px] h-[335px] flex-col justify-center items-start">
@@ -17,15 +17,18 @@ export default function Home() {
                 Right Talent with the Right Skill
               </h1>
               <p className="self-stretch font-[Inter] text-[18px] not-italic font-medium leading-[140%] text-placeholder">
-                With Solenize, finding the best talent have never been easier. Our Al software helps you to assess candidates through assessments, providing a data driven evaluation with 5x more accuracy.
+                With Solenize, finding the best talent have never been easier.
+                Our Al software helps you to assess candidates through
+                assessments, providing a data driven evaluation with 5x more
+                accuracy.
               </p>
             </div>
-            <button className="flex px-[40px] py-[15px] justify-center items-center gap-[10px] rounded-[60px] bg-[#06F6EE] hover:">
-                Get Started
+            <button className="flex px-[40px] py-[15px] justify-center items-center gap-[10px] rounded-[60px] bg-[#06F6EE] hover:bg-[#201D30] hover:text-primary transition duration-300 ease-in-out transform hover:scale-105">
+              Get Started
             </button>
           </div>
         </div>
-     </div> 
-   </main>
+      </div>
+    </main>
   );
 }
